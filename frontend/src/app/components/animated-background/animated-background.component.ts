0ad33f60356f2860c94049688df6b0459c7636af
FlorentBelotti <florent.l.d.belotti@gmail.com>
2025-07-02 00:32:02 +0200

    perlin noise noisejs test

---
+++ frontend/src/app/components/animated-background/animated-background.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';re';
+
+@Component({
+  selector: 'app-animated-background',
+  imports: [],
+  templateUrl: './animated-background.component.html',
+  styleUrl: './animated-background.component.css'
+})
+export class AnimatedBackgroundComponent {
+
+}
