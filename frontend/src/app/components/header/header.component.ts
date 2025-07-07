1009719775b7ca6bea83615af8aa01e71b9ed107
FlorentBelotti <florent.l.d.belotti@gmail.com>
2025-07-07 05:32:37 +0200

    corps begin

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterLink, RouterLinkActive } from '@angular/router'
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { AnimatedBackgroundComponent } from '../animated-background/animated-background.component';
 
 @Component({
@@ -9,6 +12,35 @@ import { AnimatedBackgroundComponent } from '../animated-background/animated-bac
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  currentTitle: string = 'WELCOME';
+  private routerSubscription: Subscription = new Subscription();
+  constructor(private router: Router) {}
 
+  private titleMap: { [key: string]: string } = {
+    '/': 'WELCOME',
+    '/resume': 'ABOUT ME',
+    '/school-projects': 'MY WORKS',
+    '/side-projects': 'MY PROJECTS',
+    '/testimonials': 'TESTIMONIALS',
+    '/contact': 'CONTACT ME',
+  };
+
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.updateTitle(event.urlAfterRedirects);
+      });
+    this.updateTitle(this.router.url);
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
+  }
+
+  private updateTitle(url: string): void {
+    const titleFromUrl = url.split('?')[0].split('#')[0];
+    this.currentTitle = this.titleMap[titleFromUrl] || this.titleMap['/'];
+  }
 }
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
+  getCurrentDate(): string {
+    return new Date().toISOString().split('T')[0].replace(/-/g, '.');
+  }
 
+  getCurrentTimestamp(): string {
+    return Date.now().toString().slice(-5);
+  }
 }
